b6880f7c9202c4f24bfbfb53438d8285a8d97671
mateusmateus22 <63922148+mateusmateus22@users.noreply.github.com>
2021-01-23 01:12:03 -0300

    Criação da página de cadastro

--- codigo/src/views/About.vue
+++ codigo/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Essa é a página de sorteios</h1>
+
   </div>
 </template>
+
+<style> 
+
+
+
+</style>
--- codigo/src/views/Cadastro.vue
+++ codigo/src/views/Cadastro.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>Cadastro</h1>
+    <form> 
+    Insiria seu nome
+    </form>
   </div>
 </template>
