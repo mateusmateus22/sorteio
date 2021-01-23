628ac7086e8db2966ec4ea2e9d29887366644d90
thiagopiassigit <thiagoprofissional29@gmail.com>
2021-01-23 01:23:26 -0300

    Update Cadastro.vue

--- codigo/src/views/Cadastro.vue
+++ codigo/src/views/Cadastro.vue
@@ -2,7 +2,8 @@
   <div class="about">
     <h1>Cadastro</h1>
     <form action="#" method ="post">
-    
+      <input type="text" placeholder="email">
+      <input type="password" placeholder="*****">
     </form>
   </div>
 </template>
