dcb1b158b8f97270a6f897752173d46d1e7b3d0b
mateusmateus22 <63922148+mateusmateus22@users.noreply.github.com>
2021-01-23 01:25:50 -0300

    Cadastro

--- codigo/src/views/Cadastro.vue
+++ codigo/src/views/Cadastro.vue
@@ -2,8 +2,11 @@
   <div class="about">
     <h1>Cadastro</h1>
     <form action="#" method ="post">
-      <input type="text" placeholder="email">
-      <input type="password" placeholder="*****">
+      <input type="text" placeholder="Nome"><br>
+      <input type="text" placeholder="email"><br>
+      <input type="password" placeholder="*****"><br>
+      <input type="submit" value="Cadastrar"><br>
+
     </form>
   </div>
 </template>
