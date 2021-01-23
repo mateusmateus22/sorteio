bd58da65a771241fed5782bfb44cb16cea5e476e
thiagopiassigit <thiagoprofissional29@gmail.com>
2021-01-23 01:18:54 -0300

    Update About.vue

--- codigo/src/views/About.vue
+++ codigo/src/views/About.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="about">
+    <img src="../assets/Sorteio+no+instagram.jpg"  width="600"/>
     <h1>Essa é a página de sorteios</h1>
-
+    <button>Relaizar sorteio</button>
   </div>
 </template>
 
-<style> 
-
-
-
+<style>
+  button {
+    border: none;
+    background-color: rgb(24, 12, 68);
+    width: 200px;
+    height: 50px;
+    color: white;
+    font-size: 18px;
+  }
 </style>
