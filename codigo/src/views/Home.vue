ac2ca71eb0718b401e6f6550996d21942d756ee1
thiagopiassigit <thiagoprofissional29@gmail.com>
2021-01-23 00:55:37 -0300

    Update Home.vue

--- codigo/src/views/Home.vue
+++ codigo/src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Bem-Vindo ao App de sorteios!"/>
   </div>
 </template>
 
